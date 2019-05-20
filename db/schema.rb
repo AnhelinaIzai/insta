21b535e4eeb629649e2eacd6bd02387b177ae21e
VikaKhusti <viktorijahusti@gmail.com>
2019-05-20 23:01:25 +0300

    add profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_180517) do
+ActiveRecord::Schema.define(version: 2019_05_20_195605) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,21 @@ ActiveRecord::Schema.define(version: 2019_05_20_180517) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.integer "user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "nickname"
+    t.integer "age"
+    t.string "avatar"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
